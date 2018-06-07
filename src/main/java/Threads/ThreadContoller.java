15f10a6d18b85965b8f29e05419e2c5356e1d50b
Soma Szolyák <soma.szolyak@gmail.com>
2018-06-07 12:58:16 +0200

    threadController is created and has todos for following work

---
+++ src/main/java/Threads/ThreadContoller.java
@@ -0,0 +1,9 @@
+package Threads;
+
+public class ThreadContoller {
+    // TODO: 2018.06.07. this creates multiple FAMTs and those read the file
+    // TODO: 2018.06.07. all FAMT instances must use the same TextFileReader
+    // TODO: 2018.06.07. need some function that copies the results of the threads together
+    // TODO: 2018.06.07. print the results
+    // TODO: 2018.06.07. stop and restart individual threads
+}
